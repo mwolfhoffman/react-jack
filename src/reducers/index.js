14983381e36d53b385f619217d43b501018e955d
Michael Hoffman <michaelhoffmanj@gmail.com>
2021-02-14 07:26:03 -0700

    fixing error from netlify build

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -118,6 +118,7 @@ export default function appReducer(state = initialState, action) {
     case actions.SET_GAME_OVER:
       return { ...state, gameOver: true };
     case actions.START_DEALER_TURN:
+      break;
     //  TODO: Need to check if dealer will hit or hold.
     //  Need to be able to communicate if next round should start.
     default:
