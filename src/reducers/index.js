419c0dafa123e2eaa01929fb0d8f05a370e96831
Michael Hoffman <michaelhoffmanj@gmail.com>
2021-02-14 07:42:11 -0700

    removing breaking prod change and starting new fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -117,8 +117,8 @@ export default function appReducer(state = initialState, action) {
       };
     case actions.SET_GAME_OVER:
       return { ...state, gameOver: true };
-    case actions.START_DEALER_TURN:
-      break;
+    // case actions.START_DEALER_TURN:
+    //   break;
     //  TODO: Need to check if dealer will hit or hold.
     //  Need to be able to communicate if next round should start.
     default:
